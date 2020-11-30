962de3968f3d27c727c60c8ee563e3a1fffdec83
Justas Titovec <justas.titovec@yahoo.com>
2020-11-30 21:38:14 +0100

    Updates

---
+++ precache-manifest.f7cf7dc9bc3f2f90fec48a2952cc1a73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c612295fb028d6d9a0b281ba7d97e4",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "4ca550499fe150ce0919",
+    "url": "/my-resume/static/css/main.15d4f192.chunk.css"
+  },
+  {
+    "revision": "8fecb0c2c597b94aaa04",
+    "url": "/my-resume/static/js/2.1c1c76c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-resume/static/js/2.1c1c76c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ca550499fe150ce0919",
+    "url": "/my-resume/static/js/main.a68c9e99.chunk.js"
+  },
+  {
+    "revision": "58c00e484c6dd359faf6",
+    "url": "/my-resume/static/js/runtime-main.9516edb6.js"
+  },
+  {
+    "revision": "bab77193cb18054eb5ea95fb884591b0",
+    "url": "/my-resume/static/media/Picture1.bab77193.png"
+  }
+]);
